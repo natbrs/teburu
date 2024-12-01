d54299cb5203be905132214cc700f8a7395a4555
Natan Almeida <83055901+natbrs@users.noreply.github.com>
2024-12-01 05:32:59 -0300

    fix auth

--- www/js/togglePass.js
+++ www/js/togglePass.js
@@ -1,31 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const eyeIcon = document.getElementById('olho')
-  const passwordField = document.getElementById('pass')
+document.addEventListener('DOMContentLoaded', function (){
+  const eyeIcon=document.getElementById('olho');
+  const passwordField=document.getElementById('pass');
 
-  const eyeIconConfirm = document.getElementById('olho-confirm')
-  const confirmPasswordField = document.getElementById('pass-confirm')
-
-  eyeIcon.addEventListener('click', function () {
-    if (passwordField.type === 'password') {
-      passwordField.type = 'text'
-      eyeIcon.classList.remove('fa-eye')
-      eyeIcon.classList.add('fa-eye-slash')
-    } else {
-      passwordField.type = 'password'
-      eyeIcon.classList.remove('fa-eye-slash')
-      eyeIcon.classList.add('fa-eye')
-    }
-  })
-
-  eyeIconConfirm.addEventListener('click', function () {
-    if (confirmPasswordField.type === 'password') {
-      confirmPasswordField.type = 'text'
-      eyeIconConfirm.classList.remove('fa-eye')
-      eyeIconConfirm.classList.add('fa-eye-slash')
-    } else {
-      confirmPasswordField.type = 'password'
-      eyeIconConfirm.classList.remove('fa-eye-slash')
-      eyeIconConfirm.classList.add('fa-eye')
-    }
-  })
-})
+  const eyeIconConfirm=document.getElementById('olho-confirm');
+  const confirmPasswordField=document.getElementById('pass-confirm');
+  if(eyeIcon){
+    eyeIcon.addEventListener('click', function(){
+      if (passwordField.type==='password'){
+        passwordField.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+      } else {
+        passwordField.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+      }
+    });
+  }
+  if(eyeIconConfirm && confirmPasswordField){
+    eyeIconConfirm.addEventListener('click', function(){
+      if(confirmPasswordField.type==='password'){
+        confirmPasswordField.type='text';
+        eyeIconConfirm.classList.remove('fa-eye');
+        eyeIconConfirm.classList.add('fa-eye-slash');
+      }else{
+        confirmPasswordField.type='password';
+        eyeIconConfirm.classList.remove('fa-eye-slash');
+        eyeIconConfirm.classList.add('fa-eye');
+      }
+    });
+  }
+});
